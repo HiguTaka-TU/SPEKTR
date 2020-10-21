488e09f99b23f0ca5c120616df741aea8a3f3533
HiguTaka-TU <higuchitakayuki.tokushima@gmail.com>
2020-10-21 14:50:10 +0000

    SPEKTR_fig

--- SPEKTR.py
+++ SPEKTR.py
@@ -26,6 +26,32 @@ def SPEKTR_fig(rawpath):
 	plt.close()
 
 
-rawpath="/mnt/nfs_S65/Takayuki/package_TotalDensityEstimation/SPEKTRspectrum/SPEKTRspectrum7500/spectrum1.text"
+def NearestSpectrum_fig(rawpath):
+	lineCount=0
+	x=[]
+	fig=plt.figure()
+	for line in open(rawpath,"r"):
+		lineCount += 1
+		if lineCount ==1:
+			continue
+		data=line.split()
+		x.append(data[2])
+	if i==1:
+		plt.plot(x,color="blue",label="No.1")
+	if i==2:
+		plt.plot(x,color="red",label="No.2")
+	if i==3:
+		plt.plot(x,color="green",label="No.3")
+	lineCount=0
 
-#SPEKTR_fig(rawpath)
+	x=[]
+	plt.title('spectrum')
+	plt.legend(loc='upper right')
+	#plt.xlabel('')
+	#plt.ylabel('')
+	plt.savefig()
+	filename='nearlySpectrum'
+	plt.savefig(filename)
+	plt.close()
+#rawpath="/mnt/nfs_S65/Takayuki/package_TotalDensityEstimation/SPEKTRspectrum/SPEKTRspectrum7500/spectrum1.text"
+#NearestSpectrum_fig(rawpath)
