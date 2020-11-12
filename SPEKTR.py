590672e920b6fbf896592f646f4c8527d35f3556
Takayuki Higuchi <takayuki@bayes.cc.h.u-tokyo.ac.jp>
2020-11-12 17:25:15 +0900

    add def

--- SPEKTR.py
+++ SPEKTR.py
@@ -1,135 +1,136 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-def SPEKTR_fig_text(rawpath):
-	lineCount=0
-	x=[]
-	fig = plt.figure()
-	for line in open(rawpath,"r"):
-		lineCount += 1
-		if lineCount ==1:
-			continue
-		data=line.split()
-		x.append(data[2])
-	plt.plot(x,color="blue")
-	lineCount=0
-
-	x=[] #reset
-
-	plt.title('spectrum')
-	#plt.legend(loc='lower right')
-	#plt.xlabel('')
-	#plt.ylabel('')
-	filename='./spectrum.png'
-	plt.savefig(filename)
-	plt.close()
-
-def SPEKTR_fig_csv():
-	csv_name = './spectrum7500_normalization.csv'
-
-	f1=np.loadtxt(csv_name,delimiter=' ')
-
-	spectrum=np.array(f1)
-
-	fig = plt.figure()
-	i=0 
-	plt.plot(spectrum[i])
-	filename='./spectrum.png'
-	plt.savefig(filename)
-	plt.close()
-
-def SPEKTR_TEXTtoCSV():
-	for i in range(1,100):
-		lineCount=0
-		x=[]
-		fig = plt.figure()
-		for line in open(rawpath,"r"):
-			lineCount += 1
-			if lineCount ==1:
-				continue
-			data=line.split()
-			x.append(data[2])
-		plt.plot(x,color="blue")
-		lineCount=0
-
-
-		with open('spectrum.csv','a') as f:
-			writer=csv.writer(f)
-			writer.writerow(x)
-		
-		x=[] #reset
-
-
-
-
-
-def NearestSpectrum_fig(rawpath):
-	lineCount=0
-	x=[]
-	fig=plt.figure()
-	for line in open(rawpath,"r"):
-		lineCount += 1
-		if lineCount ==1:
-			continue
-		data=line.split()
-		x.append(data[2])
-	if i==1:
-		plt.plot(x,color="blue",label="No.1")
-	if i==2:
-		plt.plot(x,color="red",label="No.2")
-	if i==3:
-		plt.plot(x,color="green",label="No.3")
-	lineCount=0
-
-	x=[]
-	plt.title('spectrum')
-	plt.legend(loc='upper right')
-	#plt.xlabel('')
-	#plt.ylabel('')
-	plt.savefig()
-	filename='nearlySpectrum'
-	plt.savefig(filename)
-	plt.close()
-
-def B3FQQNew_Pred_fig():
-	lineCount=0
-	x=[]
-	fig=plt.figure()
-	csv1_name = '/mnt/nfs_S65/Takayuki/SpectrumEstimation/CrossEntropy_Zscore/New_estimate/B3F.txt'
-	csv2_name = '/mnt/nfs_S65/Takayuki/SpectrumEstimation/CrossEntropy_Zscore/New_estimate/QQ.txt'
-	csv3_name = '/mnt/nfs_S65/Takayuki/SpectrumEstimation/CrossEntropy_Zscore/New_estimate/New.txt'
-
-	f1=np.loadtxt(csv1_name,delimiter=' ')
-	f2=np.loadtxt(csv2_name,delimiter=' ')
-	f3=np.loadtxt(csv3_name,delimiter=' ')
-
-	B3F=np.array(f1)
-	QQ=np.array(f2)
-	New=np.array(f3)
-
-	plt.plot(B3F,label="B3F")
-	plt.plot(QQ,label="QQ")
-	plt.plot(New,label="New")
+#データの読み込みを行う
+def load_data_csv_space(file_name):
+	data=np.loadtxt(file_name,delimiter=' ')
 	
-	plt.title('spectrum_pred')
+	return data
+
+def load_data_csv_comma(file_name):
+	data=np.loadtxt(file_name,delimiter=',')
+	
+	return data
+	
+def load_spectrum_txt(file_name):
+	data=np.loadtxt(file_name,skiprows=1)
+	
+	return data
+
+#データの書き込みを行う
+def write_csv_a(csv_name,data):
+	with open(csv_name,'a') as f:
+		writer=csv.writer(f)
+		writer.writerow(data)
+		
+def write_csv_w(csv_name,data):
+	with open(csv_name,'w') as f:
+		writer=csv.writer(f)
+		writer.writerow(data)
+
+#２つのスペクトルの比較を行い、図に表示
+def SPEKTR_compare_fig(spectrum1,specturm2):
+	label1_name='Cu_mm'
+	label2_name='Al_mm'
+	save_name='./spectrum_compare.png'
+	
+	fig = plt.figure()
+	
+	plt.plot(spectrum1[:,2],color="orange",label=label1_name,linewidth=1)
+	plt.plot(spectrum2[:,2],color="blue",label=label2_name,linewidth=1)
+	
+	plt.title('spectrum')
 	plt.legend(loc='upper right')
 	#plt.xlabel('')
 	#plt.ylabel('')
-	fig.savefig("NewEstimation_check.png")
+	plt.savefig(save_name)
+	
+	plt.close()
 
-def MeanEnergy():
-	Mean_Energy=[]
-	for i in range(1,7501):
-		rawpath="/mnt/nfs_S65/Takayuki/package_TotalDensityEstimation/SPEKTRspectrum/SPEKTRspectrum7500_normalization/spectrum_normalization%d.text" % i
+
+#スペクトルを図に表示し、保存
+def spectrum_fig(spectrum):
+	kV=140
+	save_name='./spectrum_%dkV.png' % kV
+	label_name='%dkV' % kV
+
+	spectrum=np.array(spectrum)
+	
+	fig = plt.figure()
+	
+	x=np.arange(0.5,150,1)
+	
+	plt.plot(x,spectrum[i,:],marker='^',alpha=0.5,linewidth=0.5,color='blue',markersize=3,label=label_name)
+	
+	plt.legend(loc='upper right')
+	plt.savefig(filename)
+	
+	plt.close()
+
+#データセットのためにテキストファイルをcsvファイルにまとめる
+def SPEKTR_text_to_csv(file_number,text_name,csv_name):
+	for i in range(1,file_number+1):
+	
+
+#テキストファイルからスペクトルの割合を抜き出す
+def extract_spectrum_txt():
+	#初期化
+	line_count=0
+	fraction=[]
+	for line in open(text_name,"r"):
+		line_count += 1 
+		if line_count ==1: #一行目は無視
+			continue
+		data=line.split()
+		fraction.append(data[2]) #スペクトルの割合を追加
+
+	return fraction
+
+
+#mean_energyは空の配列 mean_energy=[]
+def mean_energy(file_name,mean_energy):
+	for i in range(1,10000):
+		#i=float(i/10)
 		Mean=0
-		data=np.loadtxt(rawpath,skiprows=1)
+		data=np.loadtxt(file_name,skiprows=1)
 		for i in range(data.shape[0]):
 			Mean+=1000 * (data[i][0]+data[i][1])/2 * data[i][2]
-		print(Mean)
-		Mean_Energy.append(Mean)
+		mean_energy.append(Mean)
+
+def mean_energy_compare():
+	fig=plt.figure()
+	x1=np.arange(0.1,10.1,0.1)
+	x2=np.arange(0.1,5.1,0.1)
 	
-	with open('MeanEnergy.csv','a') as f:
-		writer=csv.writer(f)
-		writer.writerow(Mean_Energy)
-MeanEnergy()
+	
+	plt.scatter(x1,f1,marker='^',color='b',label='Al0-10mm')
+	plt.scatter(x2,f2,marker='^',color='orange',label='Cu0-5mm')
+	
+	plt.xlabel('filter thickness')
+	plt.ylabel('Mean Energy')
+	filename='MeanEnergy.png'
+	plt.legend(loc='upper right')
+	plt.savefig(filename)
+
+
+
+def mean_energy_fig():
+	x=np.arange(1,10000,1)
+
+	fig=plt.figure()
+	
+	
+	plt.scatter(x,MeanEnergy,s=10,marker='^',label='81-140kV')
+	
+	plt.xlabel('spectrum Number')
+	plt.ylabel('MeanEnergy')
+	
+	plt.title('MeanEnergy')
+	plt.legend(loc='upper left')
+	filename='MeanEnergy.png'
+	plt.savefig(filename)
+	plt.close()
+
+MeanEnergy_fig()
